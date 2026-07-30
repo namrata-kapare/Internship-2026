0f35ab5e27aede2cdc18c25cc702b2198f3698d0
namratakapare2-cloud <namratakapare2@gmail.com>
2026-07-30 23:58:58 +0530

    your commit message

---
+++ javascript/conditional.js
@@ -0,0 +1,37 @@
+// conditional opearator
+// nested if else
+//    if()
+//     {
+//          if()
+//           {
+
+//           }  
+//           else
+//          {
+
+//           }
+//      }
+//      else
+//    {
+//     }
+
+
+
+// else if ladder - more than one condition
+var a=10;
+   if(a<20)
+   {
+    console.log("if1");
+   }
+   else if(a==10)
+   {
+    console.log("if2");
+   }
+   else if(a!=20)
+   {
+    console.log("if3");
+   }
+   else
+    {
+    console.log("else");
+   }
